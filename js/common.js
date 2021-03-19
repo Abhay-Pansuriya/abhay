a0eaa570a65f1ddf5d3439e889ef23d13db49c4e
Abhay-Pansuriya <abhay.pansuriya77@gmail.com>
2021-03-20 05:29:06 +0530

    uploaded

---
+++ js/common.js
@@ -0,0 +1,8 @@
+// typing text animation script
+var typed = new Typed(".change-text", {
+    strings: ["Abhay Pansuriya","Developer", "Programmer", "Designer", "Code Lover"],
+    typeSpeed: 90,
+    backSpeed: 60,
+    loop: true
+});
+
