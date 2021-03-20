89ef6acd44d978003f412d9288c18708a73cdecb
Abhay-Pansuriya <abhay.pansuriya77@gmail.com>
2021-03-20 06:15:57 +0530

    done till about me

--- js/common.js
+++ js/common.js
@@ -1,8 +1,19 @@
-// typing text animation script
-var typed = new Typed(".change-text", {
-    strings: ["Abhay Pansuriya","Developer", "Programmer", "Designer", "Code Lover"],
-    typeSpeed: 90,
-    backSpeed: 60,
-    loop: true
-});
 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('nav').addClass("sticky");
+        }else{
+            $('nav').removeClass("sticky");
+        }
+    });
+
+    // typing text animation script
+    var typed = new Typed(".change-text", {
+        strings: ["Abhay Pansuriya","Developer", "Programmer", "Designer", "Code Lover"],
+        typeSpeed: 90,
+        backSpeed: 60,
+        loop: true
+    });
+})
