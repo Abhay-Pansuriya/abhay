f2835951f12b9c1eaf77b7faaad914ab5f8c2966
Abhay-Pansuriya <abhay.pansuriya77@gmail.com>
2021-03-21 06:52:21 +0530

    After progress bar

--- js/common.js
+++ js/common.js
@@ -16,4 +16,26 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+
+    // For Progress bar
+    $('.progress-content div .myProgress').each(function(){
+        var i = 0;
+        if (i == 0) {
+            i = 1;
+            var elem = $(this).children('.myBar');
+            var per = $(this).children('.myBar').attr('data');
+            var width = 1;
+            var id = setInterval(frame, 10);
+            function frame() {
+                if (width >= parseInt(per)) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    elem.css("width",width + "%") ;
+                    elem.siblings('.progress-text').text(width + "%");
+                }
+            }
+        }
+    });
 })
