48bec66c5453a14f386d3774cdf4ed694d67da8d
Abhay <abhay.pansuriya77@gmail.com>
2022-08-11 08:53:36 +0530

    Added slider

--- js/common.js
+++ js/common.js
@@ -1,27 +1,43 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
+
+    // Defining Slick Slider 
+
+    $('.slider-sec').slick({
+        // dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+
+
+
+
+
     var hieghtThreshold = $(".progress-content").offset();
-    var hieghtThreshold_end  = $(".progress-content").offset().top +$(".progress-content").height() ;
-    $(window).scroll(function(){
+    var hieghtThreshold_end = $(".progress-content").offset().top + $(".progress-content").height();
+    $(window).scroll(function () {
         // sticky navbar on scroll script
         var scroll = $(window).scrollTop();
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('nav').addClass("sticky");
-        }else{
+        } else {
             $('nav').removeClass("sticky");
         }
 
-        if (scroll >= hieghtThreshold && scroll <=  hieghtThreshold_end ) {
+        if (scroll >= hieghtThreshold && scroll <= hieghtThreshold_end) {
             make_progress_animation();
         } else {
             // make_progress_default(); 
         }
-        
+
     });
 
     // typing text animation script
     var typed = new Typed(".change-text", {
-        strings: ["Abhay Pansuriya","Developer", "Programmer", "Designer", "Code Lover","IT Engineer"],
+        strings: ["Abhay Pansuriya", "Developer", "Programmer", "Designer", "Code Lover", "IT Engineer"],
         typeSpeed: 110,
         backSpeed: 70,
         loop: true
@@ -29,8 +45,8 @@ $(document).ready(function(){
 
     // For Progress bar
     make_progress_animation();
-    function make_progress_animation(){
-        $('.progress-content div .myProgress').each(function(){
+    function make_progress_animation() {
+        $('.progress-content div .myProgress').each(function () {
             var i = 0;
             if (i == 0) {
                 i = 1;
@@ -44,7 +60,7 @@ $(document).ready(function(){
                         i = 0;
                     } else {
                         width++;
-                        elem.css("width",width + "%") ;
+                        elem.css("width", width + "%");
                         elem.siblings('.progress-text').text(width + "%");
                         // elem.siblings('.progress-text').css("left",(width-15) + "%");
                     }
@@ -59,19 +75,27 @@ $(document).ready(function(){
     //         elem.siblings('.progress-text').text("10%");
     //     });
     // }
-    $('.hamburger').click(function(){
-        $(this).toggleClass("is-active");
-        $(this).siblings('.nav-bar').slideToggle();
-        $(this).siblings('.logo-mob').toggle();
-    });
-    $(document).mouseup(function(e) {
-        var container = $(".nav-bar");
+
+
+    var width = $(window).width();
+    if (width < 750) {
+        $('.hamburger').click(function () {
+            $(this).toggleClass("is-active");
+            $(this).siblings('.nav-bar').slideToggle();
+            $(this).siblings('.logo-mob').toggle();
+        });
+        $('ul.nav-bar li').click(function () {
+            $('.hamburger').click();
+        });
+    }
+
+    $(document).mouseup(function (e) {
+        var container = $("nav");
         var container2 = $(".hamburger");
-        // if the target of the click isn't the container nor a descendant of the container
-        var width = $(window).width();
-        if(width < 750){
-            if ((!container.is(e.target) && container.has(e.target).length === 0) || (!container2.is(e.target) && container2.has(e.target).length === 0))
-            {
+        // if the target of the click isn't the container nor a descendant of the container       
+        if (width < 750) {
+
+            if ((!container.is(e.target) && container.has(e.target).length === 0)) {
                 $('.hamburger').removeClass("is-active");
                 $('.nav-bar').slideUp();
                 $('.logo-mob').show();
